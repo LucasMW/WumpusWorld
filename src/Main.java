f1cc49d6b127a9b53e88a2f7502ab99b1f5ca8e2
unknown <great-lucas@hotmail.com>
2015-11-05 14:41:36 -0200

    after abaffa edit

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,20 @@ public class Main
 	public static void main(String args[]) 
 	{
 		System.out.println("Wumpus World");
-		Query q1 = new Query("consult", new Term[] {new Atom("mundowumpus.pl")});
-		System.out.println("consult " + (q1 != null ? "succeeded" : "failed"));
+		Query q1 = new Query("consult", new Term[] {new Atom("C:\\Users\\Lucas\\workspace\\WumpusWorldInterface\\mundowumpus.pl")});
+		System.out.println("consult " + (q1.hasSolution() ? "succeeded" : "failed"));
 		Query q2 = new Query("iniciar_mundoteste");
+		q2.hasSolution();
 		Query q3 = new Query("tamanho_mundo(X)");
-//		Hashtable[] solution = (Hashtable[]) q2.allSolutions();
-//		if (solution != null)
-//		{
-//		for (int i = 0; i < solution.length; i++)
-//		System.out.println("X = " + solution[i].get("X"));
-//		}
+		q3.hasSolution();
+		Map<String, Term>[] solution = q3.allSolutions();
+		if (solution != null)
+		{
+		for (int i = 0; i < solution.length; i++)
+		System.out.println("X = " + solution[i].get("X"));
+		}
 
-		System.out.println(q3.isOpen());
+//		System.out.println(q3.isOpen());
 		
 
 	}
