690c5a298dcad30731080dc9b48dd0680869d2c1
unknown <great-lucas@hotmail.com>
2015-11-09 10:53:57 -0200

    some queries in main

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,36 @@ public class Main
 		System.out.println("Wumpus World");
 		Query q1 = new Query("consult", new Term[] {new Atom("C:\\Users\\Lucas\\workspace\\WumpusWorldInterface\\mundowumpus.pl")});
 		System.out.println("consult " + (q1.hasSolution() ? "succeeded" : "failed"));
-		Query q2 = new Query("iniciar_mundoteste");
-		q2.hasSolution();
-		Query q3 = new Query("move(estado(agenteLocal([2,2]),_,_,_,_),Y,Z)");
-		q3.hasSolution();
-		Map<String, Term>[] solution = q3.allSolutions();
+		Query q2 = new Query("Z = iniciar_agente");
+		showSolutions(q2,"Z");
+		
+		Query q3 = new Query("Z = iniciar_mundoteste");
+		showSolutions(q3,"Z");
+		
+		
+		Query bestAction = new Query("melhor_acao(X)");
+		bestAction.hasSolution();
+		Map<String, Term>[] solution = bestAction.allSolutions();
+		if (solution != null)
+		{
+			for (int i = 0; i < solution.length; i++)
+			{
+				System.out.println("X = " + solution[i].get("X"));
+			}
+		}
+
+	System.out.println(bestAction.isOpen());
+		
+		
+	}
+	private static void showSolutions(Query q, String whatToGet  )
+	{
+		q.hasSolution();
+		Map<String, Term>[] solution = q.allSolutions();
 		if (solution != null)
 		{
 		for (int i = 0; i < solution.length; i++)
-		System.out.println("X = " + solution[i].get("X"));
+		System.out.println(whatToGet + " = " + solution[i].get(whatToGet));
 		}
-
-//		System.out.println(q3.isOpen());
-		
-
 	}
 }
